b4d5553016711d0d8325f7cc28719e289f1ed661
tyrenix <rodionovdanil609@gmail.com>
2024-06-30 16:10:37 +0400

    fix bugs

--- src/app/[locale]/(about)/page.tsx
+++ src/app/[locale]/(about)/page.tsx
@@ -1,6 +1,6 @@
-'use server'
-
 import {PageView} from './page.view.component'
+
+export const runtime = 'edge'
 
 export default async function AboutPage() {
     return <PageView />
--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,5 +1,7 @@
 import {notFound} from 'next/navigation'
 
+export const runtime = 'edge'
+
 export default async function RestPage() {
     notFound()
 }
